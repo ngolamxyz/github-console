2c0ac043340b9cfee7be5d4881ea95f396fe81b5
Lam Ngo <lamngo@kms-technology.com>
2022-05-26 12:17:57 +0700

    Styling Search field

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './Home.css';
-import TextField from '@mui/material/TextField';
+import TextField, { textFieldClasses } from '@mui/material/TextField';
 import ResultList from './components/ResultList';
 import { useDispatch, useSelector } from 'react-redux';
 import { queryUsers, updateQuery } from './reducers/userReducer';
 import { debounce } from '@mui/material';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import styled from 'styled-components';
 
-
+const Container = styled.div `
+  display: flex;
+  flex-direction: column;
+  margin: 0 5rem;
+`
+const SearchField = styled.div `
+  display: flex;
+  justify-content: center;
+  > div {
+    width: 100%;
+    padding: 1rem;
+    font-size: 1.6rem;
+  }
+  
+`
 export default function Home() {
   const users = useSelector(state => state.users)
   const dispatch = useDispatch()
@@ -22,15 +37,17 @@ export default function Home() {
     dispatch(queryUsers(searchValue))
   })
   return (
-    <div className="Home">
+    <Container>
       <Header title="Search"></Header>
-      <TextField id="searchbox" placeholder='Enter GitHub username, i.e. gaearon'
-        onChange={handleSearch} value={users.search_query} type="text" autoFocus />
+      <SearchField>
+        <TextField id="searchbox" placeholder='Enter GitHub username, i.e. gaearon'
+          onChange={handleSearch} value={users.search_query} type="text" autoFocus />
+      </SearchField>
       <img src="/imgs/githublogo.jpg"></img>
       <img src="/imgs/githubtext.jpg"></img>
       <p>Search Enter GitHub username, i.e. gaearon Enter GitHub username and search users matching the input like Google Search, click avatars to view more details, including repositories, followers and following.  search favorite Search Favorite</p>
       <ResultList></ResultList>
       <Footer/>
-    </div>
+    </Container>
   );
 }
