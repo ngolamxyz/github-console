a7268b109239c1ff761e2481d406151a929dd282
Lam Ngo <lamngo@kms-technology.com>
2022-05-26 11:07:42 +0700

    Add styled-components and format the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,39 @@
-import { Switch, Tooltip } from "@mui/material";
+import { Switch, Tooltip, tooltipClasses } from "@mui/material";
+import styled from "styled-components";
+
+const Title = styled.h2 `
+    font-family: 'Arsenal';
+    font-size: 2.6rem;
+    font-weight: 700;
+    line-height: 3.6rem;
+`
+const Container = styled.div `
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    pading: 3.2rem 1.6rem 1.6rem;
+    height: 8.4rem;
+    background: #FFF;
+`
+const CustomizedToolTip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    fontSize: 13
+  },
+}));
 
 const label = { inputProps: { 'aria-label': 'Toggle dark mode' } };
 export default function Header(props) {
     return (
         <>
-            <h2>{props.title}</h2>
-            <div className="header-darkmode">
-                <Tooltip title="Toggle dark mode" arrow>
-                    <Switch {...label} />
-                </Tooltip>
-            </div>
+        <Container>
+            <Title>{props.title}</Title>
+            <CustomizedToolTip title="Toggle dark mode" arrow>
+                <Switch {...label} />
+            </CustomizedToolTip>
+        </Container>
         </>
     )
 }
