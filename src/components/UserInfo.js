f5e9601f5b1e080bf19845e1ed1a4b1c3d904079
Lam Ngo <lamngo@kms-technology.com>
2022-05-27 21:52:18 +0700

    style info component

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -9,18 +9,54 @@ const UserBox = styled(Paper)(({ theme }) => ({
 //   color: theme.palette.text.secondary,
 }));
 
+const StyledBox = styled(UserBox)`
+    display: flex;
+    align-items: center;
+    position: relative;
+    .avatar {
+        width: 7rem;
+        height: 7rem;
+        margin-right: 2rem;
+    }
+    .following-icon {
+        width: 2rem;
+        height: 2rem;
+        position: absolute;
+        top: 5px;
+        right: 5px;
+    }
+    .details {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    .following-details {
+        font-size: 1.2rem;
+        font-weight: normal;
+        line-height: 1.6rem;
+    }
+    .username {
+        font-weight: 700;
+        font-size: 1.6rem;
+        line-height: 24px;
+    }
+
+`
+
 export default function UserInfo({ user }) {
     return (
-        <UserBox>
-            <Avatar src={user.avatarUrl} alt={user.login} variant="rounded"/>
-            <div>
+        <StyledBox>
+            <Avatar className="avatar" src={user.avatarUrl} alt={user.login} variant="rounded"/>
+            <div className="details">
                 <div className="username">{user.login}</div>
                 <div className="following-details">
                     <div className="following-details--followers">{user.followers ? user.followers.totalCount : 0 } followers</div>
                     <div className="following-details--flowing">{user.following ? user.following.totalCount : 0 } flowings</div>
                 </div>
-
             </div>
-        </UserBox>
+            <svg className="following-icon">
+                <use xlinkHref="/imgs/sprite.svg#following-icon"/>
+            </svg>
+        </StyledBox>
     )
 }
